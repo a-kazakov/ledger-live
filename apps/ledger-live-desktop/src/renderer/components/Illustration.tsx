9616fc9902ffcc6c5771c6be6d4a9a76d4475591
Pierre-Louis Crescitz <pierrelouis.crescitz@gmail.com>
2022-07-12 11:20:00 +0200

    chore: remove useless translations

--- apps/ledger-live-desktop/src/renderer/components/Illustration.tsx
+++ apps/ledger-live-desktop/src/renderer/components/Illustration.tsx
@@ -3,13 +3,14 @@ import { ThemedComponent } from "renderer/styles/StyleProviderV3";
 import styled, { DefaultTheme, ThemeProps } from "styled-components";
 
 type Props = {
-  lightSource: any;
-  darkSource: any;
-  size: any;
+  lightSource: string;
+  darkSource: string;
+  size: number;
 };
 
-const defineStyleFromTheme = (lightAsset: any, darkAsset: any) => (p: ThemeProps<DefaultTheme>) =>
-  p.theme.colors.palette.type === "light" ? lightAsset : darkAsset;
+const defineStyleFromTheme = (lightAsset: string, darkAsset: string) => (
+  p: ThemeProps<DefaultTheme>,
+) => (p.theme.colors.palette.type === "light" ? lightAsset : darkAsset);
 
 const Illustration = ({ lightSource, darkSource, size }: Props) => {
   const Img: ThemedComponent<{}> = styled.div`
